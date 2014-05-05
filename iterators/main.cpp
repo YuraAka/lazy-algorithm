b7b0a4d0555a963b50c0ea346565577052c61573
Юра <Yura@Mercury.local>
2014-05-05 23:51:39 +0400

    Initial Commit

---
+++ iterators/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  iterators
+//
+//  Created by Юра on 05.05.14.
+//  Copyright (c) 2014 yura. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
